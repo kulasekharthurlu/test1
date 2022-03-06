aae511a8192268493d2e0965d61a0e1313da8f50
Karunakar Thurlu <karunakarthurlujava@gmail.com>
2022-03-06 11:50:40 +0530

    Adding dotenv,mongoose dependencies

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+
 const server = app.listen(PORT, () => {
     console.log(`server is running on : ${PORT} `);
 });
-//Hello World
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+//send back json data
+app.get('/api/users', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'test@gmail.com',
+            age: 30,
+        },
+        {
+            id: 2,
+            firstName: 'Jane',
+            lastName: 'Doe',
+            email: 'jane@gmail.com'
+        }
+    ])
 });
